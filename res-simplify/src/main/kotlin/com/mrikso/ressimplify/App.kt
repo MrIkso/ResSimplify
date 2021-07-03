a3ba30d3f47cb4f0dc96d2706af087cba51a53f8
MrIkso <mrikso821@gmail.com>
2021-07-03 20:36:28 +0300

    added calculation of time spent for deobfuscate resources

--- res-simplify/src/main/kotlin/com/mrikso/ressimplify/App.kt
+++ res-simplify/src/main/kotlin/com/mrikso/ressimplify/App.kt
@@ -3,6 +3,9 @@ package com.mrikso.ressimplify
 import com.github.ajalt.clikt.core.CliktCommand
 import com.github.ajalt.clikt.parameters.options.option
 import com.mrikso.ressimplify.core.Run
+import java.text.SimpleDateFormat
+import java.util.*
+import kotlin.system.measureTimeMillis
 
 class DeobfuscateCommand : CliktCommand(name = "res-simplify", help = "Deobfcuscation of resources in the apk file.") {
     private val inputFile by option("--in", "-input", help = "Input file to deobfuscation file")
@@ -10,11 +13,18 @@ class DeobfuscateCommand : CliktCommand(name = "res-simplify", help = "Deobfcusc
 
     override fun run() {
         if (!inputFile.isNullOrEmpty() && !outputFile.isNullOrEmpty()) {
-            Run().run(inputFile, outputFile)
-        }else{
+            val elapsedTime = measureTimeMillis { Run().run(inputFile, outputFile) }
+            echo("Elapsed time: ${getTimeStamp(elapsedTime)}")
+        } else {
             echo("Arguments empty or null")
         }
     }
+}
+
+fun getTimeStamp(timeinMillies: Long): String? {
+    val formatter = SimpleDateFormat("HH:mm:ss") // modify format
+    formatter.timeZone = TimeZone.getTimeZone("UTC")
+    return formatter.format(Date(timeinMillies))
 }
 
 fun main(args: Array<String>) {
